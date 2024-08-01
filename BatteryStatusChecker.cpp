f20b08c523e0ced9646e9d4e44f9d84f57880c88
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-01 10:59:40 +0530

    Update BatteryStatusChecker.cpp

--- BatteryStatusChecker.cpp
+++ BatteryStatusChecker.cpp
@@ -1,11 +1,32 @@
 #include <assert.h>
 #include <iostream>
 #include "BatteryStatusChecker.hpp"
+
 using namespace std;
+
+Language currentLanguage = ENGLISH;
+ 
+const char* getErrorMessage(const char* errorMessage) {
+  static map<string, map<Language, string>> errorMessages = {
+    {"Temperature out of range!", {
+      {ENGLISH, "Temperature out of range!"},
+      {GERMAN, "Temperatur außerhalb des zulässigen Bereichs!"}
+    }},
+    {"State of Charge out of range!", {
+      {ENGLISH, "State of Charge out of range!"},
+      {GERMAN, "Ladezustand außerhalb des zulässigen Bereichs!"}
+    }},
+    {"Charge Rate out of range!", {
+      {ENGLISH, "Charge Rate out of range!"},
+      {GERMAN, "Laderate außerhalb des zulässigen Bereichs!"}
+    }}
+  };
+  return errorMessages[errorMessage][currentLanguage].c_str();
+}
 
 bool isValueInRange(float value, float min, float max, const char* errorMessage){
   if(value < min || value > max) {
-    cout << errorMessage << endl;
+    cout << getErrorMessage(errorMessage) << endl;
     return false;
   }
   return true;
