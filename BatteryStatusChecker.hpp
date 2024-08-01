86f161ad8ebb0c7cb141cb2191f3744c2bcf795a
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-01 10:59:32 +0530

    Update BatteryStatusChecker.hpp

--- BatteryStatusChecker.hpp
+++ BatteryStatusChecker.hpp
@@ -1,7 +1,12 @@
 #ifndef BATTERYSTATUSCHECKER_HPP
 #define BATTERYSTATUSCHECKER_HPP
 
-#include "LanguageOptions.h"
+#include <map>
+#include <string>
+ 
+enum Language { ENGLISH, GERMAN }; 
+extern Language currentLanguage;
+const char* getErrorMessage(const char* errorMessage);
 
 bool isValueInRange(float value, float min, float max, const char* errorMessage);
 bool isBatteryOk(float temperature, float soc, float chargeRate);
